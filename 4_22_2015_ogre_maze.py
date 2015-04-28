c77acec4ed01b7e5797f96c7d5a6d8109fc7f09c
zmoses <zmoses93@gmail.com>
2015-04-28 13:31:30 -0700

    create_maze working

--- 4_22_2015_ogre_maze.py
+++ 4_22_2015_ogre_maze.py
@@ -2,9 +2,18 @@
 http://www.reddit.com/r/dailyprogrammer/comments/33hwwf/20150422_challenge_211_intermediate_ogre_maze/
 """
 def create_maze(input_string):
-    maze = [[input_string[x] for x in range(?)] for y in range(?)]
-
+    maze = [ [ input_string[x][y] for x in range(len(input_string[0])) ] for y in range(len(input_string)) ]
 
 if __name__ == "__main__":
-    test_maze = ''
-    create_maze(test_maze)
+    test_maze = [ '@@........',
+                  '@@O.......',
+                  '.....O.O..',
+                  '..........',
+                  '..O.O.....',
+                  '..O....O.O',
+                  '.O........',
+                  '..........',
+                  '.....OO...',
+                  '.........$' ]
+    
+    maze = create_maze(test_maze)
