cc349051600079786db472fa95ef2872b77d41ed
zmoses <zmoses93@gmail.com>
2015-04-28 12:52:12 -0700

    Adding ogre maze; finish on different computer...

---
+++ 4_22_2015_ogre_maze.py
@@ -0,0 +1,10 @@
+"""
+http://www.reddit.com/r/dailyprogrammer/comments/33hwwf/20150422_challenge_211_intermediate_ogre_maze/
+"""
+def create_maze(input_string):
+    maze = [[input_string[x] for x in range(?)] for y in range(?)]
+
+
+if __name__ == "__main__":
+    test_maze = ''
+    create_maze(test_maze)
