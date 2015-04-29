222c41bf1ea0c13d4c638342f072cb5468508d8c
zmoses <zmoses93@gmail.com>
2015-04-28 18:15:13 -0700

    Add Switched to class

--- 4_22_2015_ogre_maze.py
+++ 4_22_2015_ogre_maze.py
@@ -1,19 +1,37 @@
 """
 http://www.reddit.com/r/dailyprogrammer/comments/33hwwf/20150422_challenge_211_intermediate_ogre_maze/
 """
-def create_maze(input_string):
-    maze = [ [ input_string[x][y] for x in range(len(input_string[0])) ] for y in range(len(input_string)) ]
+
+class Maze(object):
+    def __init__(self, maze):
+        self.incomplete = self.build_maze(maze)
+        self.ogre_start = self.find_ogre() # [ [top left], [top right], [bottom left], [bottom right] ] 
+        self.complete   = []
+
+    def build_maze(self, input_string):
+        return [ [ input_string[x][y] for x in range(0, len(input_string)) ] for y in range(0, len(input_string[0])) ]
+        # self.ogre_start = self.find_ogre()
+
+    def print_maze(self, maze):
+        for y in range(0, len(maze[0])):
+            for x in range(0,len(maze)):
+                print(maze[x][y], end=' ')
+                # print('x:' + str(x) + '  y:' + str(y)) # Debugging
+            print()
+
+    def find_ogre(self):
+        for y in range(0, len(self.incomplete[0])):
+            for x in range(0,len(self.incomplete)):
+                if self.incomplete[x][y] == '@':
+                    return [ [x,y], [x+1,y], [x,y+1], [x+1,y+1] ]
+        return [-1]
 
 if __name__ == "__main__":
-    test_maze = [ '@@........',
-                  '@@O.......',
-                  '.....O.O..',
-                  '..........',
-                  '..O.O.....',
-                  '..O....O.O',
-                  '.O........',
-                  '..........',
-                  '.....OO...',
-                  '.........$' ]
-    
-    maze = create_maze(test_maze)
+    test_maze = Maze([ '..........',
+                       '..........',
+                       '..O@@O....',
+                       '...@@.....',
+                       '...OO.....',
+                       '.....OO...',
+                       '.........$' ])
+    test_maze.print_maze(test_maze.incomplete)
